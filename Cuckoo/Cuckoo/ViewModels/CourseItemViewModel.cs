374458b66849c9a9caca60c862c0f50a1a0ad1c8
cyanray <a484599279@hotmail.com>
2020-08-05 23:42:46 +0800

    Add: CourseItemViewModel (for CoursesListView.Page);

---
+++ Cuckoo/Cuckoo/ViewModels/CourseItemViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Cuckoo.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace Cuckoo.ViewModels
+{
+    public class CourseItemViewModel : BaseViewModel
+    {
+        public ObservableCollection<IListItem> Items { get; set; }
+
+        public CourseItemViewModel()
+        {
+            Title = "课课课表";
+            Items = new ObservableCollection<IListItem>();
+        }
+
+    }
+}
--- Cuckoo/Cuckoo/Views/CoursesListView.xaml.cs
+++ Cuckoo/Cuckoo/Views/CoursesListView.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Cuckoo.Models;
+using Cuckoo.ViewModels;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +15,26 @@ namespace Cuckoo.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class CoursesListView : ContentPage
     {
+        private CourseItemViewModel viewModel;
+
         public CoursesListView()
         {
             InitializeComponent();
+            BindingContext = viewModel = new CourseItemViewModel()
+            {
+                Items = new ObservableCollection<IListItem>()
+                {
+                    new GroupItem() { GroupName = "上午" },
+                    new CourseItem(){ CourseName = "船舶静力学",Classroom="A01-123" },
+                    new CourseItem(){ CourseName = "船舶材料力学",Classroom="A01-234" },
+                    new GroupItem() { GroupName = "下午" },
+                    new EmptyItem(),
+                    new CourseItem(){ CourseName = "毛泽东思想与中国特色社会主义概论",Classroom="A01-108" },
+                    new GroupItem() { GroupName = "晚上" },
+                    new EmptyItem(),
+                    new EmptyItem()
+                }
+            };
         }
     }
 }
