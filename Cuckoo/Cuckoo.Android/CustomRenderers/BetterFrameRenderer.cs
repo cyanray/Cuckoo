b7d13ed4bd4a87fc881e0d8c96f8dddf3de5533c
cyanray <a484599279@hotmail.com>
2020-08-05 23:13:47 +0800

    Add: BetterFrame;

---
+++ Cuckoo/Cuckoo.Android/CustomRenderers/BetterFrameRenderer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Graphics.Drawables;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Cuckoo.Controls;
+using Cuckoo.Droid.CustomRenderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(BetterFrame), typeof(DashedFrameRenderer))]
+namespace Cuckoo.Droid.CustomRenderers
+{
+    public class DashedFrameRenderer : VisualElementRenderer<Frame>
+    {
+        public DashedFrameRenderer(Context context) : base(context) { }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.Frame> e)
+        {
+            base.OnElementChanged(e);
+
+            var extFrame = e.NewElement as BetterFrame;
+
+            var gd = new Android.Graphics.Drawables.GradientDrawable();
+
+            gd.SetColor(extFrame.BackgroundColor.ToAndroid());
+
+            gd.SetCornerRadius((float)extFrame.CornerRadius);
+
+            if (extFrame.BorderStroke > 0)
+            {
+                // TODO: Supporting line border.
+                // TODO：Supporting custom dashed line.
+                gd.SetStroke(extFrame.BorderStroke, extFrame.BorderColor.ToAndroid(), 10f, 10f);
+            }
+
+            this.Background = gd;
+
+        }
+    }
+}
---
+++ Cuckoo/Cuckoo/Controls/BetterFrame.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Cuckoo.Controls
+{
+    public class BetterFrame : Frame
+    {
+        public int BorderStroke { get; set; }
+
+        public static readonly BindableProperty BorderStrokeProperty =
+            BindableProperty.Create(
+                        propertyName: "BorderStroke",
+                        returnType: typeof(int),
+                        declaringType: typeof(BetterFrame),
+                        defaultValue: 0,
+                        defaultBindingMode: BindingMode.TwoWay,
+                        propertyChanged: BorderStrokePropertyChanged);
+
+        private static void BorderStrokePropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var control = (BetterFrame)bindable;
+            control.BorderStroke = Convert.ToInt32(newValue);
+        }
+
+        public BetterFrame()
+        {
+            BorderStroke = 0;
+        }
+
+    }
+}
