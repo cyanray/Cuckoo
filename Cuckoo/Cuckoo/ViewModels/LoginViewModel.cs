8384e1989d21a4a8a4764c5adba80ef782806927
cyanray <a484599279@hotmail.com>
2020-08-07 09:38:04 +0800

    Update: LoginPage;

---
+++ Cuckoo/Cuckoo/ViewModels/LoginViewModel.cs
@@ -0,0 +1,44 @@
+﻿using QzSdk.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cuckoo.ViewModels
+{
+    public class LoginViewModel : BaseViewModel
+    {
+        public ObservableCollection<Province> ProvinceItems { get; set; }
+        public ObservableCollection<School> SchoolItems { get; set; }
+
+        bool isBusy = false;
+        public bool IsBusy
+        {
+            get { return isBusy; }
+            set { SetProperty(ref isBusy, value); }
+        }
+
+        public LoginViewModel()
+        {
+            ProvinceItems = new ObservableCollection<Province>();
+            SchoolItems = new ObservableCollection<School>();
+            _ = LoadProvinceItemsAsync();
+        }
+
+        private async Task LoadProvinceItemsAsync()
+        {
+            IsBusy = true;
+
+            var provinces = await Task.Run(() => { return QzSdk.Qz.GetProvinces(); });
+            foreach (var item in provinces)
+            {
+                ProvinceItems.Add(item);
+            }
+            IsBusy = false;
+
+
+        }
+    }
+}
--- Cuckoo/Cuckoo/Views/LoginPage.xaml.cs
+++ Cuckoo/Cuckoo/Views/LoginPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Cuckoo.ViewModels;
+using QzSdk.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,11 +12,30 @@ using Xamarin.Forms.Xaml;
 namespace Cuckoo.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class LoginPage : ContentView
+    public partial class LoginPage : ContentPage
     {
+        LoginViewModel viewModel;
         public LoginPage()
         {
             InitializeComponent();
+            BindingContext = viewModel = new LoginViewModel();
+
+        }
+
+        private void ProvincePicker_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var picker = (Picker)sender;
+            var selectedItem = (Province)picker.SelectedItem;
+
+            if (selectedItem != null)
+            {
+                viewModel.SchoolItems.Clear();
+                var schools = QzSdk.Qz.GetSchools().Where(x => x.AreaId == selectedItem.Id).ToList();
+                foreach (var item in schools)
+                {
+                    viewModel.SchoolItems.Add(item);
+                }
+            }
         }
     }
 }
