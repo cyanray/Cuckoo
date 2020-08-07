ab0e48b680ccd7a5ed3e192eb6187daffd484644
cyanray <a484599279@hotmail.com>
2020-08-07 21:08:18 +0800

    Add: Image Tint Effect;

---
+++ Cuckoo/Cuckoo.Android/Effects/ImageTintColorImpl.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Cuckoo.Droid.Effects;
+using Cuckoo.Effects;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportEffect(typeof(TintImageImpl), nameof(TintImage))]
+namespace Cuckoo.Droid.Effects
+{
+    public class TintImageImpl : PlatformEffect
+    {
+        protected override void OnAttached()
+        {
+            try
+            {
+                var effect = (TintImage)Element.Effects.FirstOrDefault(e => e is TintImage);
+
+                if (effect == null || !(Control is ImageView image))
+                    return;
+
+                var filter = new PorterDuffColorFilter(effect.TintColor.ToAndroid(), PorterDuff.Mode.SrcIn);
+                image.SetColorFilter(filter);
+            }
+            catch
+            {
+
+            }
+        }
+
+        protected override void OnDetached() { }
+    }
+
+}
