04a909261847e62d48ff1d230435a026c17d8f58
cyanray <a484599279@hotmail.com>
2020-08-08 09:54:47 +0800

    Add: IToast and android implement;

---
+++ Cuckoo/Cuckoo.Android/ToastImpl.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Cuckoo.Controls;
+using Cuckoo.Droid;
+
+[assembly: Xamarin.Forms.Dependency(typeof(ToastImpl))]
+namespace Cuckoo.Droid
+{
+    public class ToastImpl : IToast
+    {
+        public void LongAlert(string message)
+        {
+            Toast.MakeText(Application.Context, message, ToastLength.Long).Show();
+        }
+
+        public void ShortAlert(string message)
+        {
+            Toast.MakeText(Application.Context, message, ToastLength.Short).Show();
+        }
+    }
+}
