5f02174f7462778e41331715c9051f85279f4fa6
cyanray <a484599279@hotmail.com>
2020-08-10 05:11:28 +0800

    Add: QzSdk.Qz.GetStudentInfoAsync;

---
+++ QzSdk/Models/StudentInfo.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QzSdk.Models
+{
+    public class StudentInfo
+    {
+        [JsonProperty("xh")]
+        public string SchoolId { get; set; }
+        
+        [JsonProperty("xm")]
+        public string StudentName { get; set; }
+        
+        [JsonProperty("yxmc")]
+        public string CollegeName { get; set; }
+
+        [JsonProperty("zymc")]
+        public string Major { get; set; }
+
+        [JsonProperty("bj")]
+        public string ClassName { get; set; }
+
+        [JsonProperty("nj")]
+        public string Grade { get; set; }
+    }
+}
--- QzSdk/Qz.cs
+++ QzSdk/Qz.cs
@@ -19,6 +19,7 @@ namespace QzSdk
         static List<School> Schools { get; set; } = null;
 
         public string Token { get; set; } = null;
+        public string SchoolId { get; set; } = null;
 
         public Qz(string api_host)
         {
@@ -78,6 +79,20 @@ namespace QzSdk
             string err_msg = json["msg"].Value<string>();
             if (token == "-1") throw new Exception($"登录错误,{err_msg}");
             this.Token = token;
+            this.SchoolId = schoolId;
         }
+
+        public async Task<StudentInfo> GetStudentInfoAsync(string schoolId = null)
+        {
+            if (Token == null) throw new Exception("未登录教务系统");
+            if (schoolId == null) schoolId = this.SchoolId;
+            var request = new RestRequest($"app.do?method=getUserInfo&xh={schoolId}");
+            request.AddHeader("token", Token);
+            var response = await ApiClient.ExecuteGetAsync(request);
+            if (response.StatusCode != System.Net.HttpStatusCode.OK)
+                throw new Exception("非200状态响应");
+            return JsonConvert.DeserializeObject<StudentInfo>(response.Content);
+        }
+
     }
 }
