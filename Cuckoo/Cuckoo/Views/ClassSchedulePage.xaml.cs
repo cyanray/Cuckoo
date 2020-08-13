0da9acd8b6fb674c024249cabcfcbd627404e105
cyanray <a484599279@hotmail.com>
2020-08-13 12:26:48 +0800

    Update: Show the schedule of the day;

--- Cuckoo/Cuckoo/Views/ClassSchedulePage.xaml.cs
+++ Cuckoo/Cuckoo/Views/ClassSchedulePage.xaml.cs
@@ -19,6 +19,14 @@ namespace Cuckoo.Views
             base.OnAppearing();
             var week = await SemesterTime.GetWeekAsync();
             WeekToolbarItem.Text = $"第{week}周";
+            int w = FormatDayOfWeek(DateTime.Now.DayOfWeek);
+            this.SelectedItem = this.Children[w];
+        }
+
+        private int FormatDayOfWeek(DayOfWeek w)
+        {
+            if (w == DayOfWeek.Sunday) return 6;
+            else return (int)w - 1;
         }
 
         public async void ToolbarItem_Clicked(object sender, EventArgs e)
